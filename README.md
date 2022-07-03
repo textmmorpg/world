56d61325b924d737e0855be5b2e805082ca1d40e
Nate Schultz <ngs32@drexel.edu>
2022-07-03 17:22:14 -0400

    sphere rendering code

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # world
 Procedural world generation
+
+## Requirements
+
+```
+pip install -r requirements.txt
+```
+
+## Credits
+
+Thanks to Song Ho Anh for the sphere rendering code:  
+http://www.songho.ca/opengl/gl_sphere.html  
+http://www.songho.ca/glsl/test_sphere.html  
---
+++ render/files/AnimationMode.js
@@ -0,0 +1,22 @@
+///////////////////////////////////////////////////////////////////////////////
+// AnimationMode.js
+// ================
+// enum for animation mode
+//
+//  AUTHOR: Song Ho Ahn (song.ahn@gmail.com)
+// CREATED: 2012-07-24
+// UPDATED: 2020-09-30
+///////////////////////////////////////////////////////////////////////////////
+
+let AnimationMode = {
+    LINEAR: 0,
+    EASE_IN: 1,
+    EASE_IN2: 2, // using circle
+    EASE_OUT: 3,
+    EASE_OUT2: 4, // using circle
+    EASE_IN_OUT: 5,
+    EASE_IN_OUT2: 6, // using circle
+    BOUNCE: 7,
+    ELASTIC: 8
+};
+
