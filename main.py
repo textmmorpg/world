e21f26e62b9b103db976deff696d9f70262e92c9
Nate Schultz <ngs32@drexel.edu>
2022-07-04 16:40:55 -0400

    adjusted biome heights

--- main.py
+++ main.py
@@ -15,17 +15,25 @@ data = []
 noise_vals = []
 
 def colorHex(value):
-    if value < 10:
+    if value < 0:
         return ['03', '00', '7B'] # deep water
-    elif value < 10.075:
+    elif value < 0.075:
         return ['66', '63', 'FF'] # shallow water
-    elif value < 10.1:
+    else:
+        value = value - 0.075 # make lowest value 0
+        value *= 10 # between 0 and 1
+        value *= 0.5 # reduce max val
+        value *= 256 # convert to color range 0 to 256
+
+        # return [str(hex(int(value))).replace('0x','')[:2], '00', '00']
+
+    if value < 25:
         return ['BF', 'BA', '07'] # beach
-    elif value < 10.13:
+    elif value < 115:
         return ['07', 'A8', '04'] # field
-    elif value < 10.15:
+    elif value < 170:
         return ['0B', '5B', '02'] # forest
-    elif value < 10.17:
+    elif value < 225:
         return ['78', '7A', '6B'] # stone
     else:
         return ['F8', 'F8', 'F8'] # snowwy mountain top
@@ -41,7 +49,6 @@ def add_noise(data):
             val += 0.25 * noise2([x/xpix, y/ypix])
             val += 0.125 * noise3([x/xpix, y/ypix])
             val += 0.125 * noise4([x/xpix, y/ypix])
-            val += 10  # perlin noise is not 0 - 1
 
             # noise_vals.append(val)
             color = colorHex(val)
@@ -57,7 +64,7 @@ def add_noise(data):
 
 data = add_noise(data)
 
-# # Generate perlin noise normal distributions
+# Generate perlin noise normal distributions
 # plt.hist(noise_vals, bins=20, histtype = 'bar', facecolor = 'blue')
 # plt.show()
 
