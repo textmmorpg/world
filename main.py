04308c556db71962bfdb386aaafa69677c235c0d
Nate Schultz <ngs32@drexel.edu>
2022-07-05 16:57:42 -0400

    3d simplex attempt

--- main.py
+++ main.py
@@ -1,18 +1,82 @@
 from PIL import Image
 import numpy as np
-from random import random
-from perlin_noise import PerlinNoise
+from opensimplex import noise3, seed
 import pandas as pd
 import matplotlib.pyplot as plt
 import tqdm
+from sympy import *
+import math
+from datetime import datetime
 
-size_x, size_y = 512*2, 512*2
-height_noise = PerlinNoise(octaves=6)
-temperature_noise = PerlinNoise(octaves=6)
-precipitation_noise = PerlinNoise(octaves=6)
+size_x, size_y = 100, 100
+diameter = 175 # 162.974661726 but there might be errors in polar to cartesian conversion
+
+# def get_noise(seed):
+#     rng = np.random.default_rng(seed=seed)
+#     ix, iy, iz = rng.random(diameter), rng.random(diameter), rng.random(diameter)
+#     return noise3array(ix, iy, iz)
+
+print(datetime.now().strftime("%H:%M:%S"))
+# print('setting up height noise')
+# height_noise = get_noise(1)
+# print(datetime.now().strftime("%H:%M:%S"))
+# print('setting up temp noise')
+# temperature_noise = get_noise(2)
+# print(datetime.now().strftime("%H:%M:%S"))
+# print('setting up precip noise')
+# precipitation_noise = get_noise(3)
+
+def height_noise(x, y, z):
+    seed(0)
+    return noise3(x,y,z)
+
+def temperature_noise(x, y, z):
+    seed(1)
+    return noise3(x,y,z)
+
+def precipitation_noise(x, y, z):
+    seed(2)
+    return noise3(x,y,z)
 
 # normalizing range values
 update_range = lambda val, old_min, old_max, new_min, new_max: (((val - old_min) * (new_max - new_min)) / (old_max - old_min)) + new_min
+
+biome_color = {
+    'ocean': '134074',
+    'tundra': '669bbc',
+    'boreal forest': '4f772d',
+    'grassland': '90a955',
+    'temperate rainforest': '31572c',
+    'seasonal forest': '606c38',
+    'woodland': '283618',
+    'tropical rainforest': '132a13',
+    'savanna': 'bb9457',
+    'beach': 'e9c46a'
+}
+
+def asCartesian(rthetaphi):
+    # convert from pixel height to polar coordinate
+    theta   = update_range(rthetaphi[1], 0, size_x, 0, math.pi*2)
+    phi     = update_range(rthetaphi[2], 0, size_y, 0, math.pi)
+
+    #takes list rthetaphi (single coord)
+    r       = rthetaphi[0]
+    theta   = theta * pi/180 # to radian
+    phi     = phi * pi/180
+    x = r * sin( theta ) * cos( phi )
+    y = r * sin( theta ) * sin( phi )
+    z = r * cos( theta )
+    return [x,y,z]
+
+# def asSpherical(xyz):
+#     #takes list xyz (single coord)
+#     x       = xyz[0]
+#     y       = xyz[1]
+#     z       = xyz[2]
+#     r       =  sqrt(x*x + y*y + z*z)
+#     theta   =  acos(z/r)*180/ pi #to degrees
+#     phi     =  atan2(y,x)*180/ pi
+#     return [r,theta,phi]
 
 # get closeness to equator
 def equator_temp(x: int) -> float:
@@ -63,19 +127,6 @@ def get_biome(is_land: bool, temperature: float, precipitation: float) -> str:
     
     return 'desert'
 
-biome_color = {
-    'ocean': '134074',
-    'tundra': '669bbc',
-    'boreal forest': '4f772d',
-    'grassland': '90a955',
-    'temperate rainforest': '31572c',
-    'seasonal forest': '606c38',
-    'woodland': '283618',
-    'tropical rainforest': '132a13',
-    'savanna': 'bb9457',
-    'beach': 'e9c46a'
-}
-
 def init_df() -> pd.DataFrame:
     df = pd.DataFrame(
         columns = [
@@ -95,13 +146,23 @@ def init_df() -> pd.DataFrame:
     df['x'] = pd.Series([i for i in range(size_x) for _ in range(size_y)])
     df['y'] = pd.Series([i for _ in range(size_x) for i in range(size_y)])
 
+    print('adding 3D cartesian coordinates')
+    df['3D'] = df.apply(lambda row: asCartesian([1, row['x'], row['y']]), axis=1)
+    df['_x'] = df.apply(lambda row: row['3D'][0], axis=1)
+    df['_y'] = df.apply(lambda row: row['3D'][1], axis=1)
+    df['_z'] = df.apply(lambda row: row['3D'][2], axis=1)
+    df.to_pickle('tmp.pickle')
+
     # add noise
     print('adding height noise')
-    df['height'] = df.apply(lambda row: height_noise([row['x']/size_x, row['y']/size_y]), axis=1)
+    df['height'] = df.apply(lambda row: height_noise(row['_x'], row['_y'], row['_z']), axis=1)
+    df.to_pickle('tmp2.pickle')
     print('adding precipitation noise')
-    df['precipitation'] = df.apply(lambda row: temperature_noise([row['x']/size_x, row['y']/size_y]), axis=1)
+    df['precipitation'] = df.apply(lambda row: temperature_noise(row['_x'], row['_y'], row['_z']), axis=1)
+    df.to_pickle('tmp2.pickle')
     print('adding temperature noise')
-    df['temperature'] = df.apply(lambda row: precipitation_noise([row['x']/size_x, row['y']/size_y]), axis=1)
+    df['temperature'] = df.apply(lambda row: precipitation_noise(row['_x'], row['_y'], row['_z']), axis=1)
+    df.to_pickle('tmp2.pickle')
 
     print('normalizing ranges')
     df['height'] = df.apply(lambda row: update_range(row['height'], -0.7, 0.7, -11000, 9000), axis=1)
@@ -150,3 +211,5 @@ for _, row in tqdm.tqdm(df[df['is_land']].iterrows()):
 np_data = np.array(image_data)
 im = Image.fromarray(np_data.astype(np.uint8))
 im.save("./render/client/images/grid512.jpg")
+
+print(datetime.now().strftime("%H:%M:%S"))
