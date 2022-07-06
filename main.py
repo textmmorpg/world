6fd16a317866453d9f620c773848a6473e3ad384
Nate Schultz <ngs32@drexel.edu>
2022-07-06 18:41:16 -0400

    misc updates

--- main.py
+++ main.py
@@ -1,42 +1,35 @@
 from PIL import Image
 import numpy as np
-from opensimplex import noise3, seed
 import pandas as pd
 import matplotlib.pyplot as plt
 import tqdm
 from sympy import *
 import math
 from datetime import datetime
+from multiprocessing import Pool
+import pickle
 
 size_x, size_y = 100, 100
-diameter = 175 # 162.974661726 but there might be errors in polar to cartesian conversion
-
-# def get_noise(seed):
-#     rng = np.random.default_rng(seed=seed)
-#     ix, iy, iz = rng.random(diameter), rng.random(diameter), rng.random(diameter)
-#     return noise3array(ix, iy, iz)
+radius = 50
 
 print(datetime.now().strftime("%H:%M:%S"))
-# print('setting up height noise')
-# height_noise = get_noise(1)
-# print(datetime.now().strftime("%H:%M:%S"))
-# print('setting up temp noise')
-# temperature_noise = get_noise(2)
-# print(datetime.now().strftime("%H:%M:%S"))
-# print('setting up precip noise')
-# precipitation_noise = get_noise(3)
+print("loading noise data")
 
-def height_noise(x, y, z):
-    seed(0)
-    return noise3(x,y,z)
+noise_data = [
+    [pickle.load(open('../noise/noise1/' + str(i) + '.pickle', 'rb')) for i in range(radius*2)],
+    [pickle.load(open('../noise/noise2/' + str(i) + '.pickle', 'rb')) for i in range(radius*2)],
+    [pickle.load(open('../noise/noise3/' + str(i) + '.pickle', 'rb')) for i in range(radius*2)],
+]
+def noise(x, y, z, i):
+    return noise_data[i][int(x)][int(y)][int(z)]
 
-def temperature_noise(x, y, z):
-    seed(1)
-    return noise3(x,y,z)
-
-def precipitation_noise(x, y, z):
-    seed(2)
-    return noise3(x,y,z)
+def parallelize_dataframe(df, func, n_cores=6):
+    df_split = np.array_split(df, n_cores)
+    pool = Pool(n_cores)
+    df = pd.concat(pool.map(func, df_split))
+    pool.close()
+    pool.join()
+    return df
 
 # normalizing range values
 update_range = lambda val, old_min, old_max, new_min, new_max: (((val - old_min) * (new_max - new_min)) / (old_max - old_min)) + new_min
@@ -67,16 +60,6 @@ def asCartesian(rthetaphi):
     y = r * sin( theta ) * sin( phi )
     z = r * cos( theta )
     return [x,y,z]
-
-# def asSpherical(xyz):
-#     #takes list xyz (single coord)
-#     x       = xyz[0]
-#     y       = xyz[1]
-#     z       = xyz[2]
-#     r       =  sqrt(x*x + y*y + z*z)
-#     theta   =  acos(z/r)*180/ pi #to degrees
-#     phi     =  atan2(y,x)*180/ pi
-#     return [r,theta,phi]
 
 # get closeness to equator
 def equator_temp(x: int) -> float:
@@ -145,30 +128,42 @@ def init_df() -> pd.DataFrame:
     print('initializing')
     df['x'] = pd.Series([i for i in range(size_x) for _ in range(size_y)])
     df['y'] = pd.Series([i for _ in range(size_x) for i in range(size_y)])
+    return df
 
+def add_3d_coords(df: pd.DataFrame) -> pd.DataFrame:
+
+    print(datetime.now().strftime("%H:%M:%S"))
     print('adding 3D cartesian coordinates')
-    df['3D'] = df.apply(lambda row: asCartesian([1, row['x'], row['y']]), axis=1)
-    df['_x'] = df.apply(lambda row: row['3D'][0], axis=1)
-    df['_y'] = df.apply(lambda row: row['3D'][1], axis=1)
-    df['_z'] = df.apply(lambda row: row['3D'][2], axis=1)
-    df.to_pickle('tmp.pickle')
+    df['3D'] = df.apply(lambda row: asCartesian([radius, row['x'], row['y']]), axis=1)
+    return df
 
+def add_height_noise(df: pd.DataFrame) -> pd.DataFrame:
     # add noise
+    print(datetime.now().strftime("%H:%M:%S"))
     print('adding height noise')
-    df['height'] = df.apply(lambda row: height_noise(row['_x'], row['_y'], row['_z']), axis=1)
-    df.to_pickle('tmp2.pickle')
-    print('adding precipitation noise')
-    df['precipitation'] = df.apply(lambda row: temperature_noise(row['_x'], row['_y'], row['_z']), axis=1)
-    df.to_pickle('tmp2.pickle')
-    print('adding temperature noise')
-    df['temperature'] = df.apply(lambda row: precipitation_noise(row['_x'], row['_y'], row['_z']), axis=1)
-    df.to_pickle('tmp2.pickle')
+    df['height'] = df.apply(lambda row: noise(*row['3D'], i=0), axis=1)
+    return df
 
+def add_precipitation_noise(df: pd.DataFrame) -> pd.DataFrame:
+    print(datetime.now().strftime("%H:%M:%S"))
+    print('adding precipitation noise')
+    df['precipitation'] = df.apply(lambda row: noise(*row['3D'], i=1), axis=1)
+    return df
+
+def add_temperature_noise(df: pd.DataFrame) -> pd.DataFrame:
+    print(datetime.now().strftime("%H:%M:%S"))
+    print('adding temperature noise')
+    df['temperature'] = df.apply(lambda row: noise(*row['3D'], i=2), axis=1)
+    return df
+
+def other_processing(df: pd.DataFrame) -> pd.DataFrame:
+    print(datetime.now().strftime("%H:%M:%S"))
     print('normalizing ranges')
-    df['height'] = df.apply(lambda row: update_range(row['height'], -0.7, 0.7, -11000, 9000), axis=1)
-    df['precipitation'] = df.apply(lambda row: update_range(row['precipitation'], -0.7, 0.7, 0, 500), axis=1)
-    df['temperature'] = df.apply(lambda row: update_range(row['temperature'], -0.7, 0.7, -10, 30), axis=1)
+    df['height'] = df.apply(lambda row: update_range(row['height'], 0, 1, -11000, 9000), axis=1)
+    df['precipitation'] = df.apply(lambda row: update_range(row['precipitation'], 0, 1, 0, 500), axis=1)
+    df['temperature'] = df.apply(lambda row: update_range(row['temperature'], 0, 1, -10, 30), axis=1)
     
+    print(datetime.now().strftime("%H:%M:%S"))
     print('checking the temperature')
     # update temperature based on latitude
     df['temperature'] = df.apply(lambda row: row['temperature'] + equator_temp(row['x']), axis=1)
@@ -176,36 +171,55 @@ def init_df() -> pd.DataFrame:
     df['temperature'] = df.apply(lambda row: row['temperature'] - row['height']/2000, axis=1)
 
     # set is_land
+    print(datetime.now().strftime("%H:%M:%S"))
     print('finding land')
     df['is_land'] = df.apply(lambda row: row['height'] > 0, axis=1)
 
     # set biome
+    print(datetime.now().strftime("%H:%M:%S"))
     print('setting biome')
     df["biome"] = df.apply(lambda row: get_biome(row['is_land'], row['temperature'], row['precipitation']), axis=1)
 
     # add beaches
+    print(datetime.now().strftime("%H:%M:%S"))
     print('going to the beach')
     df['biome'] = df.apply(lambda row: add_beach(row['biome'], row['height']), axis=1)
 
     # set color
+    print(datetime.now().strftime("%H:%M:%S"))
     print('setting color')
     df["color"] = df.apply(lambda row: biome_color[row['biome']], axis=1)
 
     # save
-    df.to_pickle('world_data.pickle')
+    print(datetime.now().strftime("%H:%M:%S"))
     return df
 
 df = init_df()
-# df = pd.read_pickle('world_data.pickle')
+df = add_3d_coords(df)
+df.to_pickle('tmp.pickle')
+df = add_height_noise(df)
+df.to_pickle('tmp2.pickle')
+df = add_precipitation_noise(df)
+df.to_pickle('tmp3.pickle')
+df = add_temperature_noise(df)
+df.to_pickle('tmp4.pickle')
+# df = pd.read_pickle('tmp4.pickle')
+df['height'].hist()
+plt.savefig('heightplot.jpg')
+df = other_processing(df)
+df.to_pickle('biomes.pickle')
+
 
 print('converting data to pixels')
-image_data = [[[13,40,74] for _ in range(size_x)] for _ in range(size_y)]
-for _, row in tqdm.tqdm(df[df['is_land']].iterrows()):
-    image_data[row['x']][row['y']] = [
-        int('0x' + row['color'][0:2], 16),
-        int('0x' + row['color'][2:4], 16),
-        int('0x' + row['color'][4:6], 16)
-    ]
+image_data = [[[13,40,74] for _ in range(size_x*5)] for _ in range(size_y*5)]
+for _, row in tqdm.tqdm(df.iterrows()):
+    for x in range(5):
+        for y in range(5):
+            image_data[row['x']+x][row['y']+y] = [
+                int('0x' + row['color'][0:2], 16),
+                int('0x' + row['color'][2:4], 16),
+                int('0x' + row['color'][4:6], 16)
+            ]
 
 # write the results to an image
 np_data = np.array(image_data)
