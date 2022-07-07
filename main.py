16f0096e66c57ea2841242f67e83cbd6a88a0797
Nate Schultz <ngs32@drexel.edu>
2022-07-06 20:53:05 -0400

    something

--- main.py
+++ main.py
@@ -1,15 +1,43 @@
 from PIL import Image
 import numpy as np
 from random import random
-from perlin_noise import PerlinNoise
 import pandas as pd
 import matplotlib.pyplot as plt
 import tqdm
+import math
+from sympy import *
+import pickle
 
-size_x, size_y = 512*2, 512*2
-height_noise = PerlinNoise(octaves=6)
-temperature_noise = PerlinNoise(octaves=6)
-precipitation_noise = PerlinNoise(octaves=6)
+size_x, size_y = 100, 100
+radius = 40
+
+noise_data = [
+    [pickle.load(open('../noise/noise1/' + str(i) + '.pickle', 'rb')) for i in range(radius*2)],
+    [pickle.load(open('../noise/noise2/' + str(i) + '.pickle', 'rb')) for i in range(radius*2)],
+    [pickle.load(open('../noise/noise3/' + str(i) + '.pickle', 'rb')) for i in range(radius*2)],
+]
+
+def noise(x, y, z, i):
+    varry1 = int(random() * 6 - 3)
+    varry2 = int(random() * 6 - 3)
+    varry3 = int(random() * 6 - 3)
+    noise = noise_data[i][x + varry1][y + varry2][z + varry3]
+    print(f'{noise} - {x},{y},{z}')
+    return noise
+
+def asCartesian(rthetaphi):
+    # convert from pixel height to polar coordinate
+    theta   = update_range(rthetaphi[1], 0, size_x, 0, math.pi*2)
+    phi     = update_range(rthetaphi[2], 0, size_y, 0, math.pi)
+
+    #takes list rthetaphi (single coord)
+    r       = rthetaphi[0]
+    theta   = theta * pi/180 # to radian
+    phi     = phi * pi/180
+    x = r * sin( theta ) * cos( phi )
+    y = r * sin( theta ) * sin( phi )
+    z = r * cos( theta )
+    return [int(x),int(y),int(z)]
 
 # normalizing range values
 update_range = lambda val, old_min, old_max, new_min, new_max: (((val - old_min) * (new_max - new_min)) / (old_max - old_min)) + new_min
@@ -95,18 +123,21 @@ def init_df() -> pd.DataFrame:
     df['x'] = pd.Series([i for i in range(size_x) for _ in range(size_y)])
     df['y'] = pd.Series([i for _ in range(size_x) for i in range(size_y)])
 
+    print('adding 3D cartesian coordinates')
+    df['3D'] = df.apply(lambda row: asCartesian([radius, row['x'], row['y']]), axis=1)
+
     # add noise
     print('adding height noise')
-    df['height'] = df.apply(lambda row: height_noise([row['x']/size_x, row['y']/size_y]), axis=1)
+    df['height'] = df.apply(lambda row: noise(*row['3D'], i=0), axis=1)
     print('adding precipitation noise')
-    df['precipitation'] = df.apply(lambda row: temperature_noise([row['x']/size_x, row['y']/size_y]), axis=1)
+    df['precipitation'] = df.apply(lambda row: noise(*row['3D'], i=0), axis=1)
     print('adding temperature noise')
-    df['temperature'] = df.apply(lambda row: precipitation_noise([row['x']/size_x, row['y']/size_y]), axis=1)
+    df['temperature'] = df.apply(lambda row: noise(*row['3D'], i=0), axis=1)
 
     print('normalizing ranges')
-    df['height'] = df.apply(lambda row: update_range(row['height'], -0.7, 0.7, -11000, 9000), axis=1)
-    df['precipitation'] = df.apply(lambda row: update_range(row['precipitation'], -0.7, 0.7, 0, 500), axis=1)
-    df['temperature'] = df.apply(lambda row: update_range(row['temperature'], -0.7, 0.7, -10, 30), axis=1)
+    df['height'] = df.apply(lambda row: update_range(row['height'], 0, 1, -11000, 9000), axis=1)
+    df['precipitation'] = df.apply(lambda row: update_range(row['precipitation'], 0, 1, 0, 500), axis=1)
+    df['temperature'] = df.apply(lambda row: update_range(row['temperature'], 0, 1, -10, 30), axis=1)
     
     print('checking the temperature')
     # update temperature based on latitude
