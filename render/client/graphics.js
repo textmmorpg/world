aba1a44e81cec0e1a974600f1937c308aa550626
Nate Schultz <ngs32@drexel.edu>
2022-07-07 15:51:09 -0400

    added user cursor

--- render/client/graphics.js
+++ render/client/graphics.js
@@ -3,7 +3,8 @@ import * as THREE from 'three';
 import { OrbitControls } from './jsm/controls/OrbitControls.js';
 
 let cameraPersp, cameraOrtho, currentCamera;
-let scene, renderer, control, orbit;
+let scene, renderer, orbit;
+let sphere;
 
 init();
 render();
@@ -17,6 +18,7 @@ function init() {
 
     const aspect = window.innerWidth / window.innerHeight;
 
+    // add camera
     cameraPersp = new THREE.PerspectiveCamera( 50, aspect, 0.01, 30000 );
     cameraOrtho = new THREE.OrthographicCamera( - 600 * aspect, 600 * aspect, 600, - 600, 0.01, 30000 );
     currentCamera = cameraPersp;
@@ -26,37 +28,39 @@ function init() {
 
     scene = new THREE.Scene();
 
-    const light = new THREE.DirectionalLight( 0xf0f0f0, 2 );
-    light.position.set( 1, 1, 1 );
-    scene.add( light );
+    // add textured sphere
+    const texture_world = new THREE.TextureLoader().load( 'textures/grid512.jpg', render );
+    texture_world.anisotropy = renderer.capabilities.getMaxAnisotropy();
 
-    const ambientLight = new THREE.AmbientLight( 0x808080 ); // soft white light
+    sphere = new THREE.SphereGeometry( 200, 200, 200 );
+    const globe_material = new THREE.MeshLambertMaterial( { map: texture_world, transparent: true } );
+    const globe_mesh = new THREE.Mesh( sphere, globe_material )
+    scene.add( globe_mesh );
+
+    // add user location indicator
+    const texture_cursor = new THREE.TextureLoader().load( 'textures/cursor.png', render );
+    texture_cursor.anisotropy = renderer.capabilities.getMaxAnisotropy();
+
+    const cursor = new THREE.SphereGeometry( 5, 5, 5 );
+    const cursor_material = new THREE.MeshLambertMaterial( { map: texture_cursor, transparent: true } );
+    const cursor_mesh = new THREE.Mesh( cursor, cursor_material )
+    scene.add( cursor_mesh );
+    cursor_mesh.position.set(200,0,0);
+
+    // add lighting
+
+    // todo; add sun that rotates corresponding to in game time
+    // const light = new THREE.DirectionalLight( 0xf0f0f0, 2 );
+    // light.position.set( 1, 1, 1 );
+    // scene.add( light );
+
+    const ambientLight = new THREE.AmbientLight( 0xffffff ); // soft white light
     scene.add( ambientLight );
 
-    const texture = new THREE.TextureLoader().load( 'textures/grid512.jpg', render );
-    texture.anisotropy = renderer.capabilities.getMaxAnisotropy();
-
-    const geometry = new THREE.SphereGeometry( 200, 200, 200 );
-    const material = new THREE.MeshLambertMaterial( { map: texture, transparent: true } );
-
+    // add mouse interactions
     orbit = new OrbitControls( currentCamera, renderer.domElement );
     orbit.update();
     orbit.addEventListener( 'change', render );
-
-    // control = new TransformControls( currentCamera, renderer.domElement );
-    // control.addEventListener( 'change', render );
-
-    // control.addEventListener( 'dragging-changed', function ( event ) {
-
-    //     orbit.enabled = ! event.value;
-
-    // } );
-
-    const mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
-
-    control.attach( mesh );
-    scene.add( control );
 
     window.addEventListener( 'resize', onWindowResize );
 }
