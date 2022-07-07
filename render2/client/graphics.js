d04dd714d904aac69d117cf340afbbc5143db398
Nate Schultz <ngs32@drexel.edu>
2022-07-07 15:11:01 -0400

    added ambient light

--- render2/client/graphics.js
+++ render2/client/graphics.js
@@ -1,54 +1,85 @@
 import * as THREE from 'three';
 
-let camera, scene, renderer;
-let mesh;
+import { OrbitControls } from './jsm/controls/OrbitControls.js';
+
+let cameraPersp, cameraOrtho, currentCamera;
+let scene, renderer, control, orbit;
 
 init();
-animate();
+render();
 
 function init() {
 
-    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
-    camera.position.z = 400;
-
-    scene = new THREE.Scene();
-
-    var loader = new THREE.TextureLoader();
-    loader.load( './textures/grid512.jpg', function ( texture ) {
-
-        var geometry = new THREE.SphereGeometry( 200, 20, 20 );
-
-        var material = new THREE.MeshBasicMaterial( { map: texture } );
-        mesh = new THREE.Mesh( geometry, material );
-        scene.add( mesh );
-
-    } );
-
-    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
+
+    const aspect = window.innerWidth / window.innerHeight;
+
+    cameraPersp = new THREE.PerspectiveCamera( 50, aspect, 0.01, 30000 );
+    cameraOrtho = new THREE.OrthographicCamera( - 600 * aspect, 600 * aspect, 600, - 600, 0.01, 30000 );
+    currentCamera = cameraPersp;
+
+    currentCamera.position.set( 500, 250, 500 );
+    currentCamera.lookAt( 0, 200, 0 );
+
+    scene = new THREE.Scene();
+
+    const light = new THREE.DirectionalLight( 0xf0f0f0, 2 );
+    light.position.set( 1, 1, 1 );
+    scene.add( light );
+
+    const ambientLight = new THREE.AmbientLight( 0x808080 ); // soft white light
+    scene.add( ambientLight );
+
+    const texture = new THREE.TextureLoader().load( 'textures/grid512.jpg', render );
+    texture.anisotropy = renderer.capabilities.getMaxAnisotropy();
+
+    const geometry = new THREE.SphereGeometry( 200, 200, 200 );
+    const material = new THREE.MeshLambertMaterial( { map: texture, transparent: true } );
+
+    orbit = new OrbitControls( currentCamera, renderer.domElement );
+    orbit.update();
+    orbit.addEventListener( 'change', render );
+
+    // control = new TransformControls( currentCamera, renderer.domElement );
+    // control.addEventListener( 'change', render );
+
+    // control.addEventListener( 'dragging-changed', function ( event ) {
+
+    //     orbit.enabled = ! event.value;
+
+    // } );
+
+    const mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
+
+    control.attach( mesh );
+    scene.add( control );
 
     window.addEventListener( 'resize', onWindowResize );
 }
 
 function onWindowResize() {
 
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
+    const aspect = window.innerWidth / window.innerHeight;
+
+    cameraPersp.aspect = aspect;
+    cameraPersp.updateProjectionMatrix();
+
+    cameraOrtho.left = cameraOrtho.bottom * aspect;
+    cameraOrtho.right = cameraOrtho.top * aspect;
+    cameraOrtho.updateProjectionMatrix();
+
     renderer.setSize( window.innerWidth, window.innerHeight );
+
+    render();
 
 }
 
-function animate() {
+function render() {
 
-    requestAnimationFrame( animate );
-
-    if(mesh) {
-        mesh.rotation.x += 0.005;
-        mesh.rotation.y += 0.01;
-    }
-
-    renderer.render( scene, camera );
+    renderer.render( scene, currentCamera );
 
 }
